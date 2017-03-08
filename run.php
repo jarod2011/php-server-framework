eab6aefd5825c355ed38da653b93e9830eb48f38
Jarod2011 <xinghen_tj@qq.com>
2017-03-08 17:54:47 +0800

    init

---
+++ run.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * This's the server entrance file
+ * @author Jarod
+ */
+
+/* define all const */
+define('ROOT_PATH', realpath(__DIR__) . DIRECTORY_SEPARATOR);
+define('LOG_PATH', ROOT_PATH . 'Logs' . DIRECTORY_SEPARATOR);
+define('FRAMEWORK_PATH', ROOT_PATH . 'Framework' . DIRECTORY_SEPARATOR);
+define('CONFIG_PATH', ROOT_PATH . 'Config' . DIRECTORY_SEPARATOR);
